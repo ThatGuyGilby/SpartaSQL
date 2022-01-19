f503e986430fd96f07a23d405032aecf412e733a
ThatGuyGilby <jack.m.gilbride@gmail.com>
2022-01-19 15:43:53 +0000

    Tasks Done

--- 19 JAN 2022 Northwind Task.sql
+++ 19 JAN 2022 Northwind Task.sql
@@ -1,3 +1,4 @@
+-- Q1
 SELECT
 ProductName,
 ProductId
@@ -9,7 +10,24 @@ SELECT
 FROM Products
 ORDER BY UnitPrice;
 
+-- Q2
 SELECT
 *
 FROM Categories
 WHERE CategoryName REGEXP '^[BS]'; -- anything that begins with B or S
+
+SELECT
+*
+FROM Categories;
+
+-- Q3
+SELECT -- select the count of applicable records from orders
+COUNT(EmployeeID)
+FROM Orders
+WHERE EmployeeID IN(5, 7); -- select orders from EmployeeID 5 and 7
+
+SELECT
+*
+FROM Orders
+WHERE EmployeeID IN(5, 7) -- select orders from EmployeeID 5 and 7
+ORDER BY EmployeeID; -- order by ascending employee id value
