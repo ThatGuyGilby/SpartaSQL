72927b53b49ff8319ad2c547404e191212a92e4d
ThatGuyGilby <jack.m.gilbride@gmail.com>
2022-01-19 22:20:09 +0000

    Homework Done

---
+++ 19 JAN 2022 Northwind Homework.sql
@@ -0,0 +1,21 @@
+-- Concatenation
+SELECT
+CONCAT(FirstName, " ", LastName, " ", HomePhone) AS "Employee Name and Contact Number"
+FROM
+Employees;
+
+-- NULL
+SELECT DISTINCT
+Country
+FROM Customers
+WHERE Region IS NOT NULL;
+
+-- Arithmetic Operators
+SELECT
+UnitPrice,
+Quantity,
+Discount,
+(UnitPrice * Quantity) AS "Price Before Reductions",
+(UnitPrice * Quantity) * (1 - Discount) AS "Gross Total"
+FROM `Order Details`
+ORDER BY Discount DESC;
